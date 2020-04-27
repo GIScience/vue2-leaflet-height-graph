40f4f5aae66aa2a819bac6327512c1e78f70de43
Nicolas Abbal <nicolas.abbal@gmail.com>
2020-04-27 15:41:02 +0200

    Added debug

--- src/Vue2LeafletHeightGraph.vue
+++ src/Vue2LeafletHeightGraph.vue
@@ -35,9 +35,16 @@
             options: {
                 type: Object,
                 default: () => {}
+            },
+            debug: {
+                type: Boolean,
+                default: false
             }
         },
         mounted() {
+            if(this.debug) {
+                console.log('mounted')
+            }
             this.$nextTick(() => {
                 this.hg = L.control.heightgraph({...this.options,...{
                     // merges quick settings with options if they are defined
@@ -48,24 +55,42 @@
             })
         },
         beforeDestroy() {
+            if(this.debug) {
+                console.log('beforeDestroy')
+            }
             if(this.hg) {
                 this.hg.remove()
             }
         },
         methods: {
             updateGraph() {
+                if(this.debug) {
+                    console.log('updateGraph')
+                }
                 if(this.hg) {
                     this.hg.remove()
                 }
                 const map = this.$parent.mapObject
-                this.hg.addTo(map)
-                let p = Object.keys(this.availableParsers).includes(this.parser) ? this.parser : 'normal'
-                let dataCollections = this.availableParsers[p](this.data)
-                this.hg.addData(dataCollections)
+                let dataCollections = null;
+                try {
+                    this.hg.addTo(map)
+                    let p = Object.keys(this.availableParsers).includes(this.parser) ? this.parser : 'normal'
+                    
+                    dataCollections = this.availableParsers[p](this.data)
+                    this.hg.addData(dataCollections)
+                } catch(err) {
+                    console.log('Error in updateGraph: ')
+                    console.log(err)
+                    console.log('data', this.data)
+                    console.log('dataCollections', dataCollections)
+                }
             }
         },
         watch: {
             data: function () {
+                if(this.debug) {
+                    console.log('watch.data')
+                }
                 if(!this.data && this.hg) {
                     this.hg.remove()
                 } else {
